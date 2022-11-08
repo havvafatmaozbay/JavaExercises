760807778c26a1cd251e5d3df382af3cf6ca516d
Havva Fatma Özbay <havvafatma45@gmail.com>
2022-11-08 22:18:32 +0300

    Create Driver.java
    The code block that takes a string from user through text field and by pressing to the button prints it in the console.

---
+++ Driver.java
@@ -0,0 +1,30 @@
+import javax.swing.*;
+import java.awt.BorderLayout;
+import java.awt.event.*;
+
+public class Driver{
+		public static void main(String[] args){
+			JFrame frame = new JFrame();
+			frame.setSize(800,600);
+			frame.setLocationRelativeTo(null);
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			JPanel panel = new JPanel();
+			
+			JTextField textField = new JTextField(20);
+			panel.add(textField);
+			JButton button = new JButton();
+			button.setText("Print Text");
+			button.addActionListener(new ActionListener(){
+				public void actionPerformed(ActionEvent event){
+					System.out.println(textField.getText())
+;				}
+
+
+			});
+			panel.add(button);
+			frame.add(panel , BorderLayout.CENTER);
+			frame.setVisible(true);
+			
+		
+	}
+}
