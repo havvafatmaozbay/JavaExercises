83464c445a148f90eb5eb2b6f37e6f1e275d5204
Havva Fatma Özbay <havvafatma45@gmail.com>
2022-11-17 20:37:49 +0300

    Multi Dimensional String Array
    This program takes a string input from user and prints it as it was, multidimensional array. In this program deepToString method is used for printing the array. Normally toString works with arrays but not works wiyh multidimensional ones.

--- MultiDimensionalArrays/src/Main.java
+++ MultiDimensionalArrays/src/Main.java
@@ -1,6 +1,22 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        int row = 0;
+        int column = 0;
+        String[][] cities = new String[2][2];
+        for( row = 0; row<cities.length;row++){
+            for( column = 0; column<cities[row].length; column++){
+                Scanner scan = new Scanner(System.in);
+                cities[row][column] = scan.nextLine();
 
-        System.out.println("Hello world!");
+            }
+
+        }
+        // This method is designed for converting multidimensional arrays to strings. The simple toString() method works well for simple arrays, but doesn’t work for multidimensional arrays.
+        System.out.println(Arrays.deepToString(cities));
+
     }
+
 }
