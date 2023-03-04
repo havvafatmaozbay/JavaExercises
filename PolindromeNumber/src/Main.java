88bccab5d26ee79d002f3cabadb990019cce5089
Havva Fatma Özbay <havvafatma45@gmail.com>
2023-03-04 20:13:34 +0300

    Summary added

--- PolindromeNumber/src/Main.java
+++ PolindromeNumber/src/Main.java
@@ -1,14 +1,24 @@
 public class Main {
     static boolean isPalindrome(int number){
-        //sayının aynısının tersini oluşturucaz eğer ikisi eşitse palindrom diyeceğiz
-        int temp = number;
+        //we will create reverse of given number
+        int temp = number; //assign number to temp
         int reverseNumber = 0;
         int lastDigit;
 
         while (temp!=0){
-            lastDigit = temp%10;
-            reverseNumber =(reverseNumber*10)+lastDigit;
+            lastDigit = temp%10; //get last digit of number
+            reverseNumber =(reverseNumber*10)+lastDigit; //to create reverse number multiply by 10 and add number each time
             temp /=10;
+            //then reduce last digit of temp
+
+            //for example given number is 242 and in first step:
+            // 242%10 = 2
+            // 0 +2 = 2 => first digit of the reverse number
+            //242/10 = 24
+            //in second step:
+            //24%10 = 4
+            // (2*10) + 4 = 24
+            //24/10 = 2
         }
         return true;
     }
